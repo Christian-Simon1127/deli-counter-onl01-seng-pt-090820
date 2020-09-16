b9a9d14fc5caaf031d74b67480a2a0311dd236a6
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-16 20:19:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def line(people) 
+  if people.empty?
+    puts "The line is currently empty."
+  else
+    positions = people.map.with_index(1) {|person, index| "#{index}. #{person}"}
+    positions.unshift("The line is currently:")
+    output = positions.join(" ")
+    puts output
+  end
+end
+
+def take_a_number(current_people, new_person) 
+    positions = people.map.with_index(1) {|person, index| "#{index}. #{person}"}
+    positions.unshift("The line is currently:")
+    output = positions.join(" ")
+    puts output
+end
