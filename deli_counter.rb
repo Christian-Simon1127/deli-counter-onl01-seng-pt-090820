846505eb4622d3ea062e89fbad158c04a822c87b
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-16 20:30:57 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def line(people) 
+  if people.empty?
+    puts "The line is currently empty."
+  else
+    positions = people.map.with_index(1) {|person, index| "#{index}. #{person}"}
+    positions.unshift("The line is currently:")
+    output = positions.join(" ")
+    puts output
+  end
+end
+
+def take_a_number(current_people, new_person)
+      current_people << new_person
+      puts "Welcome, #{new_person}. You are number #{current_people.size} in line."
+end
+
+def now_serving(people)
+  if people.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{people[0]}."
+    people.shift
+  end
+end
